d1637262e3d3ef7a5fecae027fa8a26d7aabffae
T1uTiu <ahriiiiii@gmail.com>
2023-09-29 16:09:29 +0800

    Layout with code but not xml

--- app/src/main/java/com/example/learningproject/MainActivity.java
+++ app/src/main/java/com/example/learningproject/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.learningproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World");
+        textView.setTextColor(Color.BLUE);
+        textView.setTextSize(18);
+        relativeLayout.addView(textView, params);
+        setContentView(relativeLayout);
     }
 }
