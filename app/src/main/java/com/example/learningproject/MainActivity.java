c2e5a15f080c3540878299881b1eb0471c2f60bd
T1uTiu <ahriiiiii@gmail.com>
2023-09-29 16:47:50 +0800

    Set button click event

--- app/src/main/java/com/example/learningproject/MainActivity.java
+++ app/src/main/java/com/example/learningproject/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.learningproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text_view_hello_world);
-        textView.setText(R.string.hello_android);
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(view -> {
+            CharSequence tmp = textView1.getText();
+            textView1.setText(textView2.getText());
+            textView2.setText(tmp);
+            Toast.makeText(this, "交换成功", Toast.LENGTH_LONG).show();
+            new AlertDialog.Builder(this).setTitle("交换成功").setPositiveButton("确定",null).show();
+        });
 
     }
 }
