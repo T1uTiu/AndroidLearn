61846968300ae090adb065345f8e59657b74fed6
T1uTiu <ahriiiiii@gmail.com>
2023-09-29 16:16:47 +0800

    Find TextView by ID and change its text

--- app/src/main/java/com/example/learningproject/MainActivity.java
+++ app/src/main/java/com/example/learningproject/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.learningproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,18 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        RelativeLayout relativeLayout = new RelativeLayout(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);
-        TextView textView = new TextView(this);
-        textView.setText(R.string.hello_world);
-        textView.setTextColor(Color.BLUE);
-        textView.setTextSize(18);
-        relativeLayout.addView(textView, params);
-        setContentView(relativeLayout);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_view_hello_world);
+        textView.setText(R.string.hello_android);
+
     }
 }
